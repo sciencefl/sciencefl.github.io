3b8a0c4d42d6dd05207f11596592c6079eed4707
flynn <flzhang2017@163.com>
2024-11-27 00:15:56 +0800

    技术路线整理

--- docs/.vuepress/sidebar/sidebar.ts
+++ docs/.vuepress/sidebar/sidebar.ts
@@ -1,22 +1,22 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export const zhSidebar = sidebar(
-    {
-        "/":
-        [
-            {
-                text: "如何使用",
-                icon: "laptop-code",
-                link: "architecture-design/",
-            },
-        ],
+export const zhSidebar = sidebar({
+        // 应该把更精确的路径放置在前边
         "/architecture-design/":
             [
                 {
                     text: "设计原则与设计思想",
                     icon: "basic",
-                    prefix: "architecture-design/",
-                    children: ["设计原则与思想"]
+                    link: "设计原则与思想",
+                },
+            ],
+        // 必须放在最后面
+        "/":
+            [
+                {
+                    text: "如何使用",
+                    icon: "laptop-code",
+                    link: "architecture-design/",
                 },
             ],
     }
