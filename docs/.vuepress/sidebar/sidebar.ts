ee20a752c9a76751e94e8c7798906c88c69ea504
flynn <flzhang2017@163.com>
2024-11-26 22:50:59 +0800

    技术路线整理

--- docs/.vuepress/sidebar/sidebar.ts
+++ docs/.vuepress/sidebar/sidebar.ts
@@ -1,10 +1,24 @@
 import { sidebar } from "vuepress-theme-hope";
 
-export const zhSidebar = sidebar([
+export const zhSidebar = sidebar(
     {
-      text: "如何使用",
-      icon: "laptop-code",
-      link: "architecture-design/",
-    },
-  ],
+        "/":
+        [
+            {
+                text: "如何使用",
+                icon: "laptop-code",
+                link: "architecture-design/",
+            },
+        ],
+        "/architecture-design/":
+            [
+                {
+                    text: "设计原则与设计思想",
+                    icon: "basic",
+                    prefix: "architecture-design/",
+                    children: ["设计原则与思想"]
+                },
+            ],
+    }
+
 );
